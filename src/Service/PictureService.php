aaeb12470b9701c7c49e50877e19b2fb8bc7ad2a
JeremieN6 <ngoyi.jeremie@gmail.com>
2023-05-18 13:58:55 +0200

    user form complete + bdd upsate + set episode page

---
+++ src/Service/PictureService.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Service;
+
+use Exception;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class PictureService{
+
+    private $params;
+
+    public function __construct(ParameterBagInterface $params)
+    {
+        $this->params = $params;
+    }
+
+    public function add(UploadedFile $picture, ?string $folder = '', ?int $width = 150, ?int $height = 250)
+    {
+        // On donne un nouveau nom à l'image
+        $fichier = md5(uniqid(rand(), true)) . '.webp';
+
+        //On récupere les informations de l'image
+        $picture_infos = getimagesize($picture);
+
+        if($picture_infos === false){
+            throw new Exception('Format d\'image incorrect');
+        }
+
+        // On vérifie le format de l'image
+        switch($picture_infos['mime']){
+            case 'image/png':
+                $picture_source = imagecreatefrompng($picture);
+                break;
+            case 'image/jpeg':
+                $picture_source = imagecreatefromjpeg($picture);
+                break;
+            case 'image/webp':
+                $picture_source = imagecreatefromwebp($picture);
+                break;
+
+            default : throw new Exception('Format d\'image incorrect');
+        }
+
+        //On recadre l'image
+        //On recupère les dimensions
+        $imageWidth = $picture_infos[0];
+        $imageHeight= $picture_infos[1];
+
+        //On vérifie l'orientation
+        switch ($imageWidth <=> $imageHeight){
+            case -1: //portrait
+                $squareSize = $imageWidth;
+                $src_x = 0;
+                $src_y = ($imageHeight - $squareSize) / 2;
+                break;
+            case 0: //carré
+                $squareSize = $imageWidth;
+                $src_x = 0;
+                $src_y = 0;
+                break;
+            case 1: //paysage
+                $squareSize = $imageHeight;
+                $src_y = 0;
+                $src_x = ($imageWidth - $squareSize) / 2;
+                break;
+        }
+
+        //On crée une nouvelle image "vierge"
+        $resized_picture = imagecreatetruecolor($width, $height);
+
+        imagecopyresampled($resized_picture, $picture_source, 0, 0, $src_x, 
+        $src_y, $width, $height, $squareSize, $squareSize);
+
+        $path = $this->params->get('images_profil_directory') . $folder;
+
+        //On crée le dossier de destination s'il n'existe pas
+        if(!file_exists($path . '/mini/')){
+            mkdir($path . '/mini/', 0755, true);
+        }
+
+        //On stock l'image recadré
+        imagewebp($resized_picture, $path . '/mini/' . $width . 'x' . $height . '-' . $fichier);
+
+        $picture->move($path . '/', $fichier);
+
+        return $fichier;
+    }
+
+    public function delete(string $fichier, ?string $folder = '', ?int $width = 150, ?int $height = 250)
+    {
+        if($fichier !==  'default.webp'){
+            $success = false;
+            $path = $this->params->get('images_profil_directory') . $folder;
+
+            $mini = $path . '/mini/' . $width . 'x' . $height . '-' .
+            $fichier;
+
+            if(file_exists($mini)){
+                unlink($mini);
+                $success = true;
+            }
+
+            $original = $path . '/' . $fichier;
+
+            if(file_exists($original)){
+                unlink($mini);
+                $success = true;
+            }
+
+            return $success;
+
+        }
+
+        return false;
+    }
+}
+
+?>
