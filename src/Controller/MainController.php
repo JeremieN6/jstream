c285dc59e5ab5e6038735af2c8c47c96892f57f5
JeremieN6 <ngoyi.jeremie@gmail.com>
2023-05-21 21:28:49 +0200

    Page: about, contact, faq, list_anime, css folders

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,27 +2,16 @@
 
 namespace App\Controller;
 
-use App\Entity\Animes;
-use App\Entity\AnimeSaison;
 use App\Entity\Saison;
 use App\Entity\SaisonEpisodes;
-use App\Entity\Users;
-use App\Form\UserFormType;
 use App\Repository\AnimeSaisonRepository;
 use App\Repository\AnimesRepository;
 use App\Repository\EpisodeRepository;
-use App\Repository\SaisonEpisodesRepository;
 use App\Repository\SaisonRepository;
-use App\Repository\UsersRepository;
-use App\Service\PictureService;
-use Container2M96TVv\getMercuryseriesFlashy_FlashyNotifierService;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
 
 class MainController extends AbstractController
 {
@@ -173,84 +162,4 @@ class MainController extends AbstractController
         ]);
     }
     
-    #[Route('/manage-profil', name: 'modif_profil')]
-    public function manage_profil(
-    Request $request, 
-    EntityManagerInterface $em,): Response
-    {
-
-        //On récupère l'utilisateur connecté
-        $user = $this->getUser();
-        //On crée le formulaire
-        $userForm = $this->createForm(UserFormType::class, $user);
-        //On traite la requête du formulaire
-        $userForm->handleRequest($request);
-
-
-        //On vérifie si le formulaire est soumis ET valide
-        if($userForm->isSubmitted() && $userForm->isValid()){
-
-            // //On récupère mes images
-            // $images = $userForm->get('images')->getData();
-
-            // foreach($images as $image){
-            //     $folder = 'users';
-
-            //     //On appelle le service d'ajout
-            //     $fichier = $pictureService->add($image, $folder, 150, 150);
-
-            //     $img = new Images();
-
-            //     $img->setName($fichier);
-            //     $user->addImage($img);
-            // }
-
-            //On génère le slug
-            // $slug = $slugger->slug($user->getPseudo());
-            // $user->setSlug($slug);
-
-            //envoie a l'entité
-            $em->persist($user);
-            $em->flush();
-
-            $this->addFlash('success', 'Ton profil a été modifié avec succès ! 🚀');
-
-            //On redirige
-            return $this->redirectToRoute('parametres');
-        }
-
-        return $this->render('params/manage_profil.html.twig', [
-            'controller_name' => 'MainPageController',
-            'userForm' => $userForm->createView()
-        ]);
-    }
-
-    #[Route('/parametres', name: 'parametres')]
-    public function mentions(): Response
-    {
-
-        //On récupère l'utilisateur connecté
-        $connectedUser = $this->getUser();
-
-        return $this->render('params/parametres.html.twig', [
-            'controller_name' => 'MainPageController',
-            'user' => $connectedUser,
-        ]);
-    }
-
-    #[Route('/abonnement', name: 'abonnement')]
-    public function abonnement(): Response
-    {
-        return $this->render('params/abonnement.html.twig', [
-            'controller_name' => 'MainPageController',
-        ]);
-    }
-
-    #[Route('/tarifs', name: 'pricing-plan')]
-    public function tarifs(): Response
-    {
-        return $this->render('params/pricing-plan.html.twig', [
-            'controller_name' => 'MainPageController',
-        ]);
-    }
 }
